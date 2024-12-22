59522be3de8306da3fe6ba882939c43bac668654
takeo07 <miya.takeo@gmail.com>
2024-12-22 18:46:50 +0900

    雛形を登録

---
+++ src/kkmTerm/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {onMounted} from 'vue';
+import {Terminal} from '@xterm/xterm';
+import '@xterm/xterm/css/xterm.css';
+
+onMounted(() => {
+    console.log('onMounted');
+    const elem = document.getElementById('terminal')!;
+    var term = new Terminal();
+    term.open(elem);
+})
+</script>
+
+<template>
+    <div id="terminal"></div>
+</template>
+
+<style scoped>
+
+</style>
+<style>
+
+</style>
