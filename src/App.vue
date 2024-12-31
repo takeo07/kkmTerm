8154871f6c05c83ca0feadb2e961796ccfa58a29
takeo07 <miya.takeo@gmail.com>
2024-12-31 22:31:17 +0900

    初期登録

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { Terminal } from '@xterm/xterm';
+import '@xterm/xterm/css/xterm.css';
+
+onMounted(() => {
+  console.log('onMounted');
+  let term = new Terminal();
+  term.open(document.getElementById('terminal')!);
+})
+</script>
+
+<template>
+  <div id="terminal"></div>
+</template>
+
+<style scoped>
+</style>
+<style>
+
+</style>
