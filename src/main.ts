cc541eeedaa438223311648cf290c452da028424
takeo07 <miya.takeo@gmail.com>
2025-06-30 20:42:32 +0900

    add Vuetify and remove template code for tauri

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from "vue";
+
+// Vutify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount("#app");
